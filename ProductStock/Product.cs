aec9f846a9ee66b0d6315372b564eaec63302bc9
Leonardo <Leonardo@DESKTOP-496MDC1>
2022-02-23 19:42:26 -0300

    Adicionar arquivos de projeto.

---
+++ ProductStock/Product.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+
+namespace ProductStock
+{
+    class Product
+    {
+        private string _name;
+        public double Price { get; private set; }
+        public int QuantityStock { get; private set; }
+
+        public Product(string name, double price, int quantityStock)
+        {
+            this._name = name;
+            this.Price = price;
+            this.QuantityStock = quantityStock;
+        }
+
+        public Product(string name, double price)
+        {
+            this._name = name;
+            this.Price = price;
+            this.QuantityStock = 0;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set {
+                if (value != null && value.Length > 1)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public double StockValue()
+        {
+            return Price * QuantityStock;
+        }
+
+        public void AddStock(int quantity)
+        {
+            QuantityStock+= quantity;
+        }
+
+        public void RemoveStock(int quantity)
+        {
+            QuantityStock -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return _name + ", $ " +
+                   Price.ToString("F2", CultureInfo.InvariantCulture) + ", " +
+                   QuantityStock + " unidades, total: $ " +
+                   StockValue().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ProductStock/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProductStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite os dados do produto: ");
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade em estoque: ");
+            int quantityStock = int.Parse(Console.ReadLine());
+
+            Product product = new Product(name, price, quantityStock);
+
+            Console.WriteLine("Dados do produto: " + product.ToString());
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade para adicionar no estoque: ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            product.AddStock(quantity);
+
+            Console.WriteLine("Dados do produto: " + product.ToString());
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade para retirar do estoque: ");
+            quantity = int.Parse(Console.ReadLine());
+
+            product.RemoveStock(quantity);
+
+            Console.WriteLine("Dados do produto: " + product.ToString());
+        }
+    }
+}
